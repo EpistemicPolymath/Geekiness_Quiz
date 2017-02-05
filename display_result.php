3597eebb39f639c04d753ea5c2cc9c7ea02baf48
Judah Wright <judah@servercanyon.com>
2017-02-05 08:22:05 -0600

    Little bit of optimization

--- display_result.php
+++ display_result.php
@@ -6,26 +6,25 @@
  * Time: 7:31 PM
  */
 
+#Variable to store user input
+$responses = [];
 
 #Grab the user input from the form
-$q1_response = filter_input(INPUT_POST,'question_1');
-$q2_response = filter_input(INPUT_POST,'question_2');
-$q3_response = filter_input(INPUT_POST,'question_3');
-$q4_response = filter_input(INPUT_POST,'question_4');
-$q5_response = filter_input(INPUT_POST,'question_5');
-$q6_response = filter_input(INPUT_POST,'question_6');
-$q7_response = filter_input(INPUT_POST,'question_7');
-
+for ($i=1;$i<=7;$i++) {
+    $responses[$i] = filter_input(INPUT_POST,'question_'.$i);
+}
 
 
 #Create Question Arrays
-$q1_answers = array("Less than 3"=>0, "3-10"=>5, "More than 10"=>10);
-$q2_answers = array("Romance/Reality TV"=>0, "Action/Thriller/Crime"=>3, "Drama/Documentary/Natural Films"=>6, "Comedy/Animation"=>9, "Science Fiction/Fantasy"=>12);
-$q3_answers = array("Nope"=>0, "Only what I studied in school"=>5, "Of Course!"=>10);
-$q4_answers = array("Get whatever the sales person says is best for my needs."=>0, "Get a model identical or similar to that of my friends."=>2, "Ask a friend or read reviews online."=>5, "I already know what mobile I want as I follow mobile industry updates."=>10);
-$q5_answers = array("Ugh. I didn't like ANYTHING in school."=>0, "Physical Education"=>2, "Art Literature or History"=>3, "Computers, Math, or Science"=>10);
-$q6_answers = array("Buy a new one."=>0, "Take it to my usual repair shop."=>2, "Try to fix it myself with some help from the net."=>10);
-$q7_answers = array("Out at party or bar."=>0, "Gaming with friends"=>5, "Surfing the net or reading a book."=>10);
+$scores = [
+    1 => array("Less than 3"=>0, "3-10"=>5, "More than 10"=>10),
+    2 => array("Romance/Reality TV"=>0, "Action/Thriller/Crime"=>3, "Drama/Documentary/Natural Films"=>6, "Comedy/Animation"=>9, "Science Fiction/Fantasy"=>12),
+    3 => array("Nope"=>0, "Only what I studied in school"=>5, "Of Course!"=>10),
+    4 => array("Get whatever the sales person says is best for my needs."=>0, "Get a model identical or similar to that of my friends."=>2, "Ask a friend or read reviews online."=>5, "I already know what mobile I want as I follow mobile industry updates."=>10),
+    5 => array("Ugh. I didn't like ANYTHING in school."=>0, "Physical Education"=>2, "Art Literature or History"=>3, "Computers, Math, or Science"=>10),
+    6 => array("Buy a new one."=>0, "Take it to my usual repair shop."=>2, "Try to fix it myself with some help from the net."=>10),
+    7 => array("Out at party or bar."=>0, "Gaming with friends"=>5, "Surfing the net or reading a book."=>10)
+];
 
 #Create answers array
 
